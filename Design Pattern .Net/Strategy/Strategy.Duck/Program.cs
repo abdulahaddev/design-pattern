9d1f7ddc7c9ceb849402cd63ea3859ea7e8cee6a
ahad <ministerahad@gmail.com>
2024-07-08 00:53:45 +0600

    Initial Commit

---
+++ Design Pattern .Net/Strategy/Strategy.Duck/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Strategy.Duck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Design Pattern Java/Design Pattern/strategy_pattern/duck/Main.java
@@ -0,0 +1,9 @@
+package duck;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
